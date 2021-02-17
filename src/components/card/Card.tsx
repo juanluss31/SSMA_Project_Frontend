9a3dd45401ab2e12282c250eb54b98ebb792b38d
Daniel Nuez Wehbe <alu0100881165@ull.edu.es>
2021-02-17 14:33:20 +0000

    Creado formulario de login, se adapta si es movil o web

--- src/components/card/Card.tsx
+++ src/components/card/Card.tsx
@@ -1,40 +1,52 @@
-import { IonCard, IonCardContent, IonCol, IonIcon, IonInput, IonItem, IonLabel, IonList, IonRow } from "@ionic/react"
+import { IonCard, IonCardContent, IonCol, IonGrid, IonIcon, IonInput, IonItem, IonLabel, IonList, IonRow, IonButtons, IonButton } from '@ionic/react';
 import { personCircle } from "ionicons/icons";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import { useMediaQuery } from 'react-responsive';
 
 import './Card.scss';
 
 const Card: React.FC = () => {
 
     const [username, setUsername] = useState<string>();
+    const [password, setPassword] = useState<string>();
+    const [formStyle, setFormStyle] = useState<string>();
+
+    const isMobile = useMediaQuery({ query: `(max-width: 699px)` });
+
+    useEffect(() => {
+        isMobile ? setFormStyle("inputFormMobile") : setFormStyle("inputFormWeb");
+    }, [])
 
     return(
-        <div>
-            <IonCard>
-                <IonCardContent className="inputForm">
-                <IonRow>
-                    <IonCol>
-                        <IonIcon
-                        style={{ fontSize: "70px", color: "#0040ff" }}
-                        icon={personCircle}
-                        />
-                    </IonCol>
-                </IonRow>
-                <IonRow>
-                    <IonCol>
-                        <IonList>
-                            <IonItem>
-                                <IonLabel position="floating">Usuario:</IonLabel>
-                                <IonInput value={username} placeholder="Introduzca su usuario" onIonChange={(e) => setUsername(e.detail.value!)} clearInput required pattern="text">
-                                    {/* <IonLabel>Usuario:</IonLabel> */}
-                                </IonInput>
-                            </IonItem>
-                        </IonList>
-                    </IonCol>
-                </IonRow>
-                </IonCardContent>
-            </IonCard>
-        </div>
+        <IonCard className={formStyle}>
+            <IonCardContent>
+                <IonGrid>
+                    <IonRow>
+                        <IonCol>
+                            <IonIcon
+                            style={{ fontSize: "70px", color: "#37bfd1" }}
+                            icon={personCircle}
+                            />
+                        </IonCol>
+                    </IonRow>
+                    <IonRow className="marginRow">
+                        <IonCol>
+                            <IonList>
+                                <IonItem>
+                                    <IonLabel position="floating">Usuario:</IonLabel>
+                                    <IonInput value={username} placeholder="Introduzca su usuario" onIonChange={(e) => setUsername(e.detail.value!)} clearInput required pattern="text" />
+                                </IonItem>
+                                <IonItem>
+                                <IonLabel position="floating">Contraseña:</IonLabel>
+                                    <IonInput value={password} placeholder="Introduzca su contraseña" onIonChange={(e) => setPassword(e.detail.value!)} clearInput required pattern="text" />
+                                </IonItem>
+                            </IonList>
+                        </IonCol>
+                    </IonRow>
+                </IonGrid>
+                <IonButton style={{color: "#FFFFFF"}}>Acceder</IonButton>
+            </IonCardContent>
+        </IonCard>
     )
 }
 
